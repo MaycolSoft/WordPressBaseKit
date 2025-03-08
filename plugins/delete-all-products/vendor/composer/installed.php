11b888c5a804b017199a637cc3504c91b4014e80
mbaret <mbaret@minicode.com.do>
2025-03-08 13:26:38 -0500

    feat(delete-all-products): add initial plugin structure and files

---
+++ plugins/delete-all-products/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1046ce731ca535c850d92bb1bd6c8b040eb08f96',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1046ce731ca535c850d92bb1bd6c8b040eb08f96',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
